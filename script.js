3ef92dc841aa0e59b01104c379450e13eba66880
dtquoc1202 <quoccccc36@gmail.com>
2024-10-25 01:45:31 +0700

    First Push after finish header & hero-section

---
+++ script.js
@@ -0,0 +1,9 @@
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuClosenButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+  // Toggle mobile menu visibility
+  document.body.classList.toggle("show-mobile-menu")
+});
+// Close menu when the close button is clicked
+menuClosenButton.addEventListener("click", () => menuOpenButton.click());
